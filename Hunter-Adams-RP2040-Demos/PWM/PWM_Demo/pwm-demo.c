0e0b9d6c81c8eff5ca463fdaef4415914f4d1cc1
Dalton Luce <daltonluce42@gmail.com>
2025-04-14 15:02:55 -0400

    clean up

--- Hunter-Adams-RP2040-Demos/PWM/PWM_Demo/pwm-demo.c
+++ Hunter-Adams-RP2040-Demos/PWM/PWM_Demo/pwm-demo.c
@@ -54,60 +54,11 @@ volatile Point active_point;
 volatile float current_angle; // Track the current angle of the motor in radians
 #define RAD_PER_STEP 0.0534
 
-// PWM wrap value and clock divide value
-// For a CPU rate of 125 MHz, this gives
-// a PWM frequency of 1 kHz.
-#define WRAPVAL 5000
-#define CLKDIV 25.0f
-
-// GPIO we're using for PWM
-#define PWM_OUT 4
 // GPIO for timing the ISR
 #define ISR_GPIO 15
-// GPIO for Motor, + subsequent 3 pins
-// So this uses GPIO 2,3,4,5
+// GPIO for motor
+// IMPORTANT: Uses this + subsequent 3 pins, so this uses GPIO 2,3,4,5
 #define MOTOR2_IN1 2
-
-// Variable to hold PWM slice number
-uint slice_num ;
-
-// PWM duty cycle
-volatile int control ;
-volatile int old_control ;
-
-// PWM interrupt service routine
-void on_pwm_wrap() {
-
-    // Assert a GPIO when we enter the interrupt
-    gpio_put(ISR_GPIO, 1) ;
-
-    // Clear the interrupt flag that brought us here
-    pwm_clear_irq(pwm_gpio_to_slice_num(PWM_OUT));
-
-    // PT_SEM_SIGNAL(pt, &vga_semaphore);
-
-    // De-assert the GPIO when we leave the interrupt
-    gpio_put(ISR_GPIO, 0) ;
-}
-
-// User input thread
-static PT_THREAD (protothread_serial(struct pt *pt))
-{
-    PT_BEGIN(pt) ;
-    static int test_in ;
-    while(1) {
-        sprintf(pt_serial_out_buffer, "input a duty cycle (0-5000): ");
-        serial_write ;
-        // spawn a thread to do the non-blocking serial read
-        serial_read ;
-        // convert input string to number
-        sscanf(pt_serial_in_buffer,"%d", &test_in) ;
-        if (test_in > 5000) continue ;
-        else if (test_in < 0) continue ;
-        else control = test_in ;
-    }
-    PT_END(pt) ;
-}
 
 // character array
 char screentext[200];
@@ -178,45 +129,8 @@ int main() {
     // VGA CONFIGURATION -------------------------------------------------------
     initVGA();
 
-    // Setup the ISR-timing GPIO
-    gpio_init(ISR_GPIO) ;
-    gpio_set_dir(ISR_GPIO, GPIO_OUT);
-    gpio_put(ISR_GPIO, 0) ;
-
     active_point.distance = 0;
     active_point.angle = 0.0;
-
-    ////////////////////////////////////////////////////////////////////////
-    ///////////////////////// PWM CONFIGURATION ////////////////////////////
-    ////////////////////////////////////////////////////////////////////////
-    // // Tell GPIO PWM_OUT that it is allocated to the PWM
-    // gpio_set_function(PWM_OUT, GPIO_FUNC_PWM);
-
-    // // Find out which PWM slice is connected to GPIO PWM_OUT (it's slice 2)
-    // slice_num = pwm_gpio_to_slice_num(PWM_OUT);
-
-    // // Mask our slice's IRQ output into the PWM block's single interrupt line,
-    // // and register our interrupt handler
-    // pwm_clear_irq(slice_num);
-    // pwm_set_irq_enabled(slice_num, true);
-    // irq_set_exclusive_handler(PWM_IRQ_WRAP, on_pwm_wrap);
-    // irq_set_enabled(PWM_IRQ_WRAP, true);
-
-    // // This section configures the period of the PWM signals
-    // pwm_set_wrap(slice_num, WRAPVAL) ;
-    // pwm_set_clkdiv(slice_num, CLKDIV) ;
-
-    // // Invert?
-    // // First argument is slice number.
-    // // If second argument is true, channel A is inverted.
-    // // If third argument is true, channel B is inverted.
-    // pwm_set_output_polarity (slice_num, 0, 1);
-
-    // // This sets duty cycle
-    // pwm_set_chan_level(slice_num, PWM_CHAN_A, 3125);
-
-    // // Start the channel
-    // pwm_set_mask_enabled((1u << slice_num));
 
     ////////////////////////////////////////////////////////////////////////
     ///////////////////////////// ROCK AND ROLL ////////////////////////////
@@ -229,6 +143,7 @@ int main() {
     pt_add_thread(protothread_vga) ;
     pt_schedule_start ;
 
+    // IMPORTANT: must loop here
     while (true) {
     }
 }
