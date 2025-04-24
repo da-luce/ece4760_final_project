5b4899d3184ef3f8e13f78983d3819dcb1165f8d
Dalton Luce <daltonluce42@gmail.com>
2025-04-23 21:54:29 -0400

    add overall program state

---
+++ Hunter-Adams-RP2040-Demos/src/button.h
@@ -0,0 +1,63 @@
+typedef enum {
+  NOT_PRESSED,
+  MAYBE_PRESSED,
+  PRESSED,
+  MAYBE_NOT_PRESSED
+} ButtonState;
+ButtonState button_state = NOT_PRESSED;
+
+typedef struct 
+{
+    uint gpio;
+    ButtonState state;
+    void (*on_press)(void);     // Function on press
+    void (*on_release)(void);   // Function on release
+} Button;
+
+/* Debounce button press
+ */
+bool check_button(Button *button)
+{
+    bool button_reading = gpio_get(button->gpio);
+
+    switch (button_state) {
+        case NOT_PRESSED:
+            if (button_reading == 0) {
+                button->state = MAYBE_PRESSED;
+            }
+            break;
+
+        case MAYBE_PRESSED:
+            if (button_reading == 0) {
+                button->state = PRESSED;
+
+                // Press function
+                button->on_press();
+
+            } else {
+                button->state = NOT_PRESSED;
+            }
+            break;
+
+        case PRESSED:
+            if (button_reading == 1) {
+                button->state = MAYBE_NOT_PRESSED;
+            }
+            break;
+
+        case MAYBE_NOT_PRESSED:
+            if (button_reading == 0) {
+                button->state = PRESSED;
+            } else {
+                button->state = NOT_PRESSED;
+
+                // Release function
+                button->on_release();
+
+            }
+                break;
+
+        default:
+            button->state = NOT_PRESSED;
+    }
+}
--- Hunter-Adams-RP2040-Demos/src/main.c
+++ Hunter-Adams-RP2040-Demos/src/main.c
@@ -1,10 +1,10 @@
 /**
  * V. Hunter Adams (vha3@cornell.edu)
  * PWM demo code with serial input
- * 
+ *
  * This demonstration sets a PWM duty cycle to a
  * user-specified value.
- * 
+ *
  * HARDWARE CONNECTIONS
  *     MOTOR
  *   - GPIO 2 -> IN1
@@ -13,9 +13,9 @@
  *   - GPIO 3 -> IN4
  *   - +3.3 V or 5V power supply to + terminal on driver
  *   - REMEMBER TO GROUND DRIVER
- * 
+ *
  *     VGA (as usual)
- * 
+ *
  *   - GPIO 28 -> Button -> Ground
  */
 // STD Libraries
@@ -40,6 +40,9 @@
 #include "motor_library.h"
 #include "pt_cornell_rp2040_v1_3.h"
 
+// Local libraries
+#include "button.h"
+
 // UART Setup
 #define UART_ID uart1     // Need to use uart1 since debugger probe is uart0
 #define BAUD_RATE 115200  // IMPORTANT: make sure this matches the Arduino baud rate
@@ -50,6 +53,10 @@
 // Only certain GPIO may be used for UART
 #define UART_TX_PIN 8
 #define UART_RX_PIN 9
+
+// GPIO for motor
+// IMPORTANT: Uses this + subsequent 3 pins, so this uses GPIO 2,3,4,5
+#define MOTOR2_IN1 2
 
 // Screen parameters
 #define SCREEN_Y 480 // screen height
@@ -64,161 +71,37 @@
 // VGA semaphore
 static struct pt_sem vga_semaphore;
 
-// Store points in polar form
-typedef struct 
-{
-    int distance; // In millimeters
-    float angle;  // In radians
-} Point;
+volatile int16_t current_distance = COUNTERCLOCKWISE;   // Track the current distance measurement
+volatile float current_angle      = 0.0;                // Track the current angle of the motor in radians
+volatile int current_direction    = 0;                  // Track the current direction of the motor
 
-volatile int16_t current_distance;  // Track the current distance measurement
-volatile float current_angle;       // Track the current angle of the motor in radians
-volatile int current_direction;     // Track the current direction of the motor
-
-// GPIO for timing the ISR
-#define ISR_GPIO 15
-// GPIO for motor
-// IMPORTANT: Uses this + subsequent 3 pins, so this uses GPIO 2,3,4,5
-#define MOTOR2_IN1 2
-#define BUTTON_PIN 28 // GPIO Pin for BUTTON
-
-// Program states (controlled by button)
-typedef enum {
-  ACTIVE,
-  RESET
-} ProgState;
-volatile ProgState program_state = ACTIVE;
-
-typedef enum {
-  NOT_PRESSED,
-  MAYBE_PRESSED,
-  PRESSED,
-  MAYBE_NOT_PRESSED
-} ButtonState;
-ButtonState button_state = NOT_PRESSED;
-
-/* Debounce the button press
+/* Overall program states
  */
-bool check_button()
-{
-  bool button_reading = gpio_get(BUTTON_PIN);
-
-  switch (button_state) {
-    case NOT_PRESSED:
-      if (button_reading == 0) {
-        button_state = MAYBE_PRESSED;
-      }
-      break;
-
-    case MAYBE_PRESSED:
-      if (button_reading == 0) {
-        button_state = PRESSED;
-
-        // Reset
-        program_state = RESET;
-
-      } else {
-        button_state = NOT_PRESSED;
-      }
-      break;
-
-    case PRESSED:
-      if (button_reading == 1) {
-        button_state = MAYBE_NOT_PRESSED;
-      }
-      break;
-
-    case MAYBE_NOT_PRESSED:
-      if (button_reading == 0) {
-        button_state = PRESSED;
-      } else {
-        button_state = NOT_PRESSED;
-      }
-      break;
-
-    default:
-        button_state = NOT_PRESSED;
-  }
-}
-
-// character array
-char screentext[200];
-// Thread that draws to VGA display
-static PT_THREAD (protothread_vga(struct pt *pt))
-{
-    // Indicate start of thread
-    PT_BEGIN(pt) ;
-
-    while (true) {
-
-        // Wait on semaphore
-        PT_SEM_WAIT(pt, &vga_semaphore);
-
-        // Check button for presses
-        check_button();
-
-        if (program_state == RESET)
-        {
-            fillRect(0, 0, SCREEN_X, SCREEN_Y, BLACK);
-            program_state = ACTIVE;
-        }
-
-        // Draw active point
-        int dist = current_distance;
-        float angle = current_angle;
-        // FIXME: is this correct. WTH is going on here.
-        int x_pixel = CENTER_X + (int) (dist * PX_PER_MM * cos(angle));
-        int y_pixel = CENTER_Y - (int) (dist * PX_PER_MM * sin(angle));
-
-        drawPixel(x_pixel, y_pixel, BLUE);
-
-        float angle_deg = angle * 180.0 / M_PI;
-
-        // Display textual info
-        setTextColor2(WHITE, BLACK);
-        setTextSize(1);
-        sprintf(screentext, "Distance (mm): %d      ", dist);
-        setCursor(10, 10) ;
-        writeString(screentext) ;
-        sprintf(screentext, "Angle (deg):   %f      ", angle_deg) ;
-        setCursor(10, 20);
-        writeString(screentext);
-    }
-
-    // Indicate end of thread
-    PT_END(pt);
-}
-
-uint8_t rx_buf[2];
-int received = 0;
-#define TERMINATING_CHAR '\n' // This is what sendInt16() in the Arduino program uses
-/* This is called every time we recieve a byte over the UART channel. Here, we are
- * only recieving two bytes that form a int16_t (current distance reading in mm).
- */ 
-void on_uart_rx() {
-    while (uart_is_readable(UART_ID)) {
-        uint8_t ch = uart_getc(UART_ID);
-        if (ch == '\n') {
-            if (received == TERMINATING_CHAR) {
-                int16_t dist = (rx_buf[0]) | (rx_buf[1] << 8);
-                current_distance = dist;
-                printf("Received int: %d\n", dist);
-            }
-            received = 0;
-        } else if (received < 2) {
-            rx_buf[received] = ch;
-            received += 1;
-        } else {
-            printf("Error, more than 2 bytes recieved before newline.");
-            received = 0;
-        }
-    }
-}
+typedef enum {
+  WAITING1,         // Boot into this state, this is when we could be calibrating the sensor
+  ZEROING,          // Hold button to move to zero (TODO: OR USE A SWITCH TO DETERMINE ZERO)
+  WAITING2,         // Wait to statt collecting data
+  LIDAR             // Start collecting data, next button press goes back to waiting
+} ProgramState;
+ProgramState prog_state = WAITING1;
 
 // NOTE: This is called every time the motor finishes executing a command.
 // Thus, we call our measurement stuff here...
 void pio1_interrupt_handler() {
-    pio_interrupt_clear(pio_1, 0) ;
+    pio_interrupt_clear(pio_1, 0);
+
+    if (prog_state == ZEROING)
+    {
+        // If zeroing, just move, nothing else
+        MOVE_STEPS_MOTOR_2(1);
+        return;
+    }
+
+    // Only move otherwise if we are in LiDAR mode
+    if (prog_state != LIDAR)
+    {
+        return;
+    }
 
     // 1. Increment angle representation (switch direction if required)
     if (current_direction == CLOCKWISE)
@@ -256,15 +139,153 @@ void pio1_interrupt_handler() {
     MOVE_STEPS_MOTOR_2(1);
 }
 
+// Button to clear screen
+volatile bool clear_screen = false; // Flag read by VGA thread
+void clear_button_on_press(void) {
+    clear_screen = true;
+}
+Button clear_button = {
+    .gpio = 28,
+    .state = NOT_PRESSED,
+    .on_press = clear_button_on_press,
+    .on_release = NULL,
+};
+
+// Button to manage program state
+void state_button_on_press(void) {
+    switch (prog_state) {
+        case WAITING1:
+            prog_state = ZEROING;
+            // Start motor
+            pio1_interrupt_handler();
+            break;
+        case ZEROING:
+            // This shouldn't happen
+            break;
+        case WAITING2:
+            prog_state = LIDAR;
+            // Start motor ahgain
+            pio1_interrupt_handler();
+            break;
+        case LIDAR:
+            prog_state = WAITING1;
+            break;
+        default:
+            printf("An error has occured.");
+            break;
+    }
+}
+void state_button_on_release(void) {
+    switch (prog_state) {
+        case WAITING1:
+            // This shouldn't happen
+            break;
+        case ZEROING:
+            prog_state = WAITING2;
+            break;
+        case WAITING2:
+            // This shouldn't happen
+            break;
+        case LIDAR:
+            // This shouldn't happen
+            break;
+        default:
+            printf("An error has occured.");
+            break;
+    }
+}
+Button state_button = {
+    .gpio = 29,
+    .state = NOT_PRESSED,
+    .on_press = state_button_on_press,
+    .on_release = state_button_on_release,
+};
+
+// character array
+char screentext[200];
+// Thread that draws to VGA display
+static PT_THREAD (protothread_vga(struct pt *pt))
+{
+    // Indicate start of thread
+    PT_BEGIN(pt) ;
+
+    while (true) {
+
+        // Wait on semaphore
+        PT_SEM_WAIT(pt, &vga_semaphore);
+
+        // Check buttons for press
+        check_button(&clear_button);
+        check_button(&state_button);
+
+        if (clear_screen)
+        {
+            fillRect(0, 0, SCREEN_X, SCREEN_Y, BLACK);
+            clear_screen = false;
+        }
+
+        // Draw active point
+        int dist = current_distance;
+        float angle = current_angle;
+        // FIXME: is this correct. WTH is going on here.
+        int x_pixel = CENTER_X + (int) (dist * PX_PER_MM * cos(angle));
+        int y_pixel = CENTER_Y - (int) (dist * PX_PER_MM * sin(angle));
+
+        drawPixel(x_pixel, y_pixel, BLUE);
+
+        float angle_deg = angle * 180.0 / M_PI;
+
+        // Display textual info
+        setTextColor2(WHITE, BLACK);
+        setTextSize(1);
+        sprintf(screentext, "Distance (mm): %d      ", dist);
+        setCursor(10, 10) ;
+        writeString(screentext) ;
+        sprintf(screentext, "Angle (deg):   %f      ", angle_deg) ;
+        setCursor(10, 20);
+        writeString(screentext);
+    }
+
+    // Indicate end of thread
+    PT_END(pt);
+}
+
+uint8_t rx_buf[2];
+int received = 0;
+#define TERMINATING_CHAR '\n' // This is what sendInt16() in the Arduino program uses
+/* This is called every time we recieve a byte over the UART channel. Here, we are
+ * only recieving two bytes that form a int16_t (current distance reading in mm).
+ */
+void on_uart_rx() {
+    while (uart_is_readable(UART_ID)) {
+        uint8_t ch = uart_getc(UART_ID);
+        if (ch == '\n') {
+            if (received == TERMINATING_CHAR) {
+                int16_t dist = (rx_buf[0]) | (rx_buf[1] << 8);
+                current_distance = dist;
+                printf("Received int: %d\n", dist);
+            }
+            received = 0;
+        } else if (received < 2) {
+            rx_buf[received] = ch;
+            received += 1;
+        } else {
+            printf("Error, more than 2 bytes recieved before newline.");
+            received = 0;
+        }
+    }
+}
+
 int main() {
 
     // Initialize stdio
     stdio_init_all();
 
-    // NOTE: this needs to be called before VGA setup! This will use DMA 
+    // NOTE: this needs to be called before VGA setup! This will use DMA
     // channels 0 - 9, and VGA will claim the remaining 2
     // STEPPER CONFIGURATION ---------------------------------------------------
     setupMotor2(MOTOR2_IN1, pio1_interrupt_handler) ;
+    SET_DIRECTION_MOTOR_2(current_direction);
 
     // VGA CONFIGURATION -------------------------------------------------------
     initVGA();
@@ -291,22 +312,18 @@ int main() {
     // Now enable the UART to send interrupts - RX only
     uart_set_irq_enables(UART_ID, true, false);
 
-    current_direction = COUNTERCLOCKWISE; // Is this correct?
-    current_distance = 0;
-    current_angle = 0.0;
+    // Init buttons
+    gpio_init(clear_button.gpio);
+    gpio_set_dir(clear_button.gpio, GPIO_IN);
+    gpio_pull_up(clear_button.gpio);
 
-    // Map BUTTON to GPIO port, make it low
-    gpio_init(BUTTON_PIN);
-    gpio_set_dir(BUTTON_PIN, GPIO_IN);
-    gpio_pull_up(BUTTON_PIN);
+    gpio_init(state_button.gpio);
+    gpio_set_dir(state_button.gpio, GPIO_IN);
+    gpio_pull_up(state_button.gpio);
 
     ////////////////////////////////////////////////////////////////////////
     ///////////////////////////// ROCK AND ROLL ////////////////////////////
     ////////////////////////////////////////////////////////////////////////
-
-    // IMPORTANT: start the motor!
-    SET_DIRECTION_MOTOR_2(current_direction);
-    pio1_interrupt_handler();
 
     // pt_add_thread(protothread_serial) ;
     pt_add_thread(protothread_vga) ;
