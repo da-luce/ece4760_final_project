152901dc86240e222029736678a794f2e05b73ed
Dalton Luce <daltonluce42@gmail.com>
2025-05-12 19:23:57 -0400

    add stupid effect

--- am2589_dcl252_mpm297/highlight-todo.lua
+++ am2589_dcl252_mpm297/highlight-todo.lua
@@ -1,6 +1,9 @@
 function Str(el)
     if el.text:match("TODO:") then
-      return pandoc.Span(el.text, {class = "todo"})
+      return pandoc.Span(pandoc.Str(el.text), {class = "todo"})
+    elseif el.text:lower():match("picoscope") then
+      return pandoc.Span(pandoc.Str(el.text), {class = "picoscope-highlight"})
     end
+    return nil
   end
   
