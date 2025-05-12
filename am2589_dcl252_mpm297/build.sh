db303cc63a730cd2563d6fcc74f5446f52f81625
Dalton Luce <daltonluce42@gmail.com>
2025-05-12 14:43:35 -0400

    add comments in build.sh

--- am2589_dcl252_mpm297/build.sh
+++ am2589_dcl252_mpm297/build.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
 # Convert index.md to index.html using Pandoc
+
+# Allow Pandoc to look for resources (like images) in the current directory
+# Embed all linked resources (images, etc.)
+# Generate a standalone HTML file
+# Link external CSS file
+# Enable MathJax support for rendering LaTeX-style math expressions
+# Generate a table of contents
+# Apply Zenburn code syntax highlighting
+# Highlight "TODO:" comments in the text
+
 pandoc index.md -o index.html \
     --resource-path=. \
     --embed-resources \
