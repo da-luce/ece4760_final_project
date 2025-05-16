efcc98201f4f3cfa445935257a2078fffe5348ff
Dalton Luce <daltonluce42@gmail.com>
2025-05-15 21:18:45 -0400

    rename directories (again

--- am2589_dcl252_mpm297/highlight-todo.lua
+++
@@ -1,9 +0,0 @@
-function Str(el)
-    if el.text:match("TODO:") then
-      return pandoc.Span(pandoc.Str(el.text), {class = "todo"})
-    elseif el.text:lower():match("picoscope") then
-      return pandoc.Span(pandoc.Str(el.text), {class = "picoscope-highlight"})
-    end
-    return nil
-  end
-  
